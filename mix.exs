a8764c8576aa45cfb2a84f2e09b43062bd7f9ef3
Tomas Korcak <tomas.korcak@only1.app>
2023-01-26 23:20:53 +0100

    enable ci for master branch

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ElMagicoLabs.Cache.MixProject do
     [
       app: :el_magico_cache,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      # elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
